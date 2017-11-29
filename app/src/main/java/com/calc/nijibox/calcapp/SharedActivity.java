cdffdc11146a64ff6e27f73ea4e04af6e1612588
Tsutou Takehara <tsutoutakehara@gmail.com>
2017-11-29 17:27:52 +0900

    Resolve Shared Preferences View

--- app/src/main/java/com/calc/nijibox/calcapp/SharedActivity.java
+++ app/src/main/java/com/calc/nijibox/calcapp/SharedActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 public class SharedActivity extends AppCompatActivity {
 
     private TextView textView;
+    String logd;
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
@@ -29,8 +31,12 @@ public class SharedActivity extends AppCompatActivity {
         });
 
         SharedPreferences data = getSharedPreferences(btn.MyPREFERENCES, Context.MODE_PRIVATE);
+        logd = data.getString(btn.value,"none");
 
-        System.out.println(data);
+        textView = findViewById(R.id.textView);
+        StringBuilder stb = new StringBuilder();
+        stb.append(logd);
+        textView.setText(stb);
     }
 
 }
